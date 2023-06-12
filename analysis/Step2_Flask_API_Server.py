ca6e6bd4dbafd67424f9e7fe21200358f7a28fc5
cam1lle <117128707+cam1lle@users.noreply.github.com>
2023-06-11 23:52:51 -0600

    working graphs with dataset

--- analysis/Step2_Flask_API_Server.py
+++ analysis/Step2_Flask_API_Server.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
+from datetime import date
 
 # Create the extension
 db = SQLAlchemy()
@@ -18,11 +19,15 @@ db.init_app(app)
 # Define the model for the daily_values table
 class DailyValue(db.Model):
     __tablename__ = 'daily_values'
-    Date = db.Column(db.Date, primary_key=True)
-    Value = db.Column(db.Float)
+    row_number = db.Column(db.Float, primary_key=True)
+    date = db.Column(db.Date)
+    total_vaccinations = db.Column(db.Float)
+    daily_vaccinations = db.Column(db.Float)
+    people_vaccinated_per_hundred = db.Column(db.Float)
+    location = db.Column(db.String)
 
     def __repr__(self):
-        return f"<DailyValue(Date='{self.Date}', Value='{self.Value}')>"
+        return f"<DailyValue(date='{self.date}', total_vaccinations='{self.total_vaccinations}')>"
 
 @app.route("/")
 def home():
@@ -30,12 +35,21 @@ def home():
         "<h2>United States COVID-19 Vaccination Tracker</h2>"
         "Available Routes:<br>"
         "&nbsp;&nbsp;/data"
+        "&nbsp;&nbsp;/data"
+        "&nbsp;&nbsp;/data"
     )
 
 @app.route("/data")
 def return_data():
     data = DailyValue.query.all()
-    rows = [{'Date': row.Date.strftime('%Y-%m-%d'), 'Value': row.Value} for row in data]
+    print({"data length":len(data)})
+    rows = [{
+        'Date': row.date, 
+        'Total Vaccinations': row.total_vaccinations, 
+        'Daily Vaccinations': row.daily_vaccinations, 
+        'Vaccination Rate': row.people_vaccinated_per_hundred,
+        'State': row.location
+    } for row in data]
     return jsonify(rows)
 
 if __name__ == "__main__":
---
+++ analysis/Step3_Javascript/app.js
@@ -0,0 +1,242 @@
+// Fetch the data from the Flask API
+function fetchData(url) {
+  return fetch(url)
+    .then(response => response.json())
+    .catch(error => {
+      console.error('Error:', error);
+    });
+}
+
+function unpack(rows, key) {
+  return rows.map(function(row) { return row[key]; });
+}
+
+// Generate a line chart for total vaccinations over time
+function generateLineChart(data) {
+  // Prepare the data for the line chart
+  let chartData = data.map(row => ({
+    Date: new Date(row.Date),
+    TotalVaccinations: row["Total Vaccinations"]
+  }));
+
+  // Sort the data by date in ascending order
+  chartData.sort((a, b) => a.Date - b.Date);
+
+  // Set up the line chart layout
+  let layout = {
+    title: "Total Vaccinations Over Time",
+    xaxis: {
+      title: "Date",
+      type: "date"
+    },
+    yaxis: {
+      title: "Total Vaccinations"
+    }
+  };
+
+  // Create the line chart trace
+  let trace = {
+    x: chartData.map(row => row.Date),
+    y: chartData.map(row => row.TotalVaccinations),
+    type: "scatter",
+    mode: "lines",
+    name: "Total Vaccinations"
+  };
+
+  // Generate the line chart
+  Plotly.newPlot("line-chart", [trace], layout);
+}
+
+// Generate a bar chart for daily vaccinations by state
+function generateBarChart(data) {
+  // Prepare the data for the bar chart
+  let chartData = data.map(row => ({
+    State: row.State,
+    DailyVaccinations: row["Daily Vaccinations"]
+  }));
+
+  // Sort the data by daily vaccinations in descending order
+  chartData.sort((a, b) => b.DailyVaccinations - a.DailyVaccinations);
+
+  // Set up the bar chart layout
+  let layout = {
+    title: "Daily Vaccinations by State",
+    xaxis: {
+      title: "State"
+    },
+    yaxis: {
+      title: "Daily Vaccinations"
+    }
+  };
+
+  // Create the bar chart trace
+  let trace = {
+    x: chartData.map(row => row.State),
+    y: chartData.map(row => row.DailyVaccinations),
+    type: "bar",
+    name: "Daily Vaccinations"
+  };
+
+  // Generate the bar chart
+  Plotly.newPlot("bar-chart", [trace], layout);
+}
+
+// Generate a map visualization of vaccination rates
+function generateMap(data) {
+  // Prepare the data for the map visualization
+  let mapData = data.map(row => ({
+    State: stateCodes[row.State],
+    VaccinationRate: row["Vaccination Rate"]
+  }));
+
+  let state_count = {}
+
+  let filtered_data = {}
+
+  for(let i = 0; i < mapData.length; i++){
+    if(mapData[i].VaccinationRate){
+      if(filtered_data[mapData[i].State] == undefined){
+        filtered_data[mapData[i].State] = 0
+      }
+      if(state_count[mapData[i].State] == undefined){
+        state_count[mapData[i].State] = 0
+      }
+      state_count[mapData[i].State] += 1
+      filtered_data[mapData[i].State] += mapData[i].VaccinationRate
+    }
+  }
+
+  console.log(filtered_data)
+
+  output_list = []
+
+  for( key in filtered_data){
+    filtered_data[key] = (filtered_data[key]/state_count[key])
+    output_list.push({
+      State: key,
+      VaccinationRate: filtered_data[key]
+    })
+  }
+
+
+  // Set up the map layout
+  let layout = {
+    title: "Vaccination Rates by State",
+    geo: {
+      scope: "usa",
+      projection: {
+        type: "albers usa"
+      },
+      showlakes: true,
+      lakecolor: 'rgb(255,255,255)'
+    }
+  };
+
+  console.log(output_list)
+
+  console.log(unpack(output_list, 'State'))
+  console.log(unpack(output_list, 'VaccinationRate'))
+
+  // Create the map trace
+  let output_data = [{
+    type: "choropleth",
+    locations: unpack(output_list, 'State'),
+    z: unpack(output_list, 'VaccinationRate'),
+    locationmode: "USA-states",
+    colorscale: [
+      [0, 'rgb(242,240,247)'], [0.2, 'rgb(218,218,235)'],
+      [0.4, 'rgb(188,189,220)'], [0.6, 'rgb(158,154,200)'],
+      [0.8, 'rgb(117,107,177)'], [1, 'rgb(84,39,143)']
+    ],
+    colorbar: {
+      title: "Vaccination Rate"
+    }
+  }];
+
+  // Generate the map visualization
+  Plotly.newPlot("choroplethMap", output_data, layout);
+}
+// Event listener for the button click
+d3.select("#button").on("click", function() {
+  // Fetch the data from the Flask API
+  fetchData("http://127.0.0.1:5000/data")
+    .then(data => {
+      console.log(data)
+      // Generate the selected visualization based on the fetched data
+      let visualization = document.getElementById("visualization").value;
+      switch (visualization) {
+        case "line-chart":
+          generateLineChart(data);
+          break;
+        case "bar-chart":
+          generateBarChart(data);
+          break;
+        case "choroplethMap":
+          console.log("here")
+          generateMap(data);
+          break;
+        default:
+          console.error("Invalid visualization selected");
+      }
+    });
+});
+
+
+const stateCodes = {
+  Alabama: "AL",
+  Alaska: "AK",
+  Arizona: "AZ",
+  Arkansas: "AR",
+  California: "CA",
+  Colorado: "CO",
+  Connecticut: "CT",
+  Delaware: "DE",
+  Florida: "FL",
+  Georgia: "GA",
+  Hawaii: "HI",
+  Idaho: "ID",
+  Illinois: "IL",
+  Indiana: "IN",
+  Iowa: "IA",
+  Kansas: "KS",
+  Kentucky: "KY",
+  Louisiana: "LA",
+  Maine: "ME",
+  Maryland: "MD",
+  Massachusetts: "MA",
+  Michigan: "MI",
+  Minnesota: "MN",
+  Mississippi: "MS",
+  Missouri: "MO",
+  Montana: "MT",
+  Nebraska: "NE",
+  Nevada: "NV",
+  "New Hampshire": "NH",
+  "New Jersey": "NJ",
+  "New Mexico": "NM",
+  "New York": "NY",
+  "North Carolina": "NC",
+  "North Dakota": "ND",
+  Ohio: "OH",
+  Oklahoma: "OK",
+  Oregon: "OR",
+  Pennsylvania: "PA",
+  "Rhode Island": "RI",
+  "South Carolina": "SC",
+  "South Dakota": "SD",
+  Tennessee: "TN",
+  Texas: "TX",
+  Utah: "UT",
+  Vermont: "VT",
+  Virginia: "VA",
+  Washington: "WA",
+  "West Virginia": "WV",
+  Wisconsin: "WI",
+  Wyoming: "WY",
+  "American Samoa": "AS",
+  "District of Columbia": "DC",
+  Guam: "GU",
+  "Northern Mariana Islands": "MP",
+  "Puerto Rico": "PR",
+  "U.S. Virgin Islands": "VI"
+};
